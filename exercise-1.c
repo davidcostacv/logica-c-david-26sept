36d4b7d80eef04eb2d57b1ffe33daffd669b6606
davidcostacv <davidcostacv@gmail.com>
2023-09-28 11:03:32 -0400

    Finish

--- exercise-1.c
+++ exercise-1.c
@@ -2,6 +2,20 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+   int i, numero, cont=0;
+
+   printf("Digite un numero: ");
+   scanf("%i", &numero);
+
+   for (i = 1; i <= numero; i++ ){
+      if (numero %i==0)
+         cont++;
+
+   }
+   if (cont>2) {
+      printf("El numero %i no es primo\n", numero );
+   }else {
+      printf("El numero %i es primo\n", numero);
+   }
+return 0;
 }
